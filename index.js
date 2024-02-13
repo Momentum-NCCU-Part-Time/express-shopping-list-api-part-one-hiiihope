c7e56687fda9ef1e68321677729aa7cf5ff51bd9
Ross <rhope11@yahoo.com>
2024-02-13 17:43:32 -0500

    WIP add day look for crash

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // Set up mongoose connection
 const express = require("express");
+const dayjs = require("dayjs")
 const mongoose = require("mongoose");
 const ShoppingList = require("./models/ShoppingList");
 
+
+const port = process.env.PORT;
+
+
 const app = express();
-const PORT = 3000;
-
-
 app.use(express.json());
 
 
 
-mongoose.connect("mongodb+srv://rhope11:<Paypayday3030>@cluster00.iundvi3.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://rhope11:<>@cluster00.iundvi3.mongodb.net/?retryWrites=true&w=majority")
 mongoose.set("strictQuery", false);
 const mongoDB = "";
+{ "protectConnectionStrings": true }
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -25,5 +28,20 @@ app.post("/food", async (req, response) => {
 })
 
 
-app.listen(PORT, () => console.log('Example app is listening on port 3000.'));
+app.listen(port, () => console.log('Example app is listening on port 3000.'));
 // const ShoppingLists = require("./models/ShoppingLists");
+
+// get
+app.get('/shoppinglists', (req, res) => {
+  ShoppingList.find().then((results) => {
+    if (results) {
+      res.status(200).json(results)
+    }else {
+      res.status(404).json({ message: 'not found'})
+    }
+    })
+    .catch((error) => res.status(400).json({ message: 'Bad request' }))
+})
+
+
+app.listen(port, () => console.log(`Application is running port ${port}`))
